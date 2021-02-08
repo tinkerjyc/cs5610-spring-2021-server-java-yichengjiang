d79f96217058e41cecc140e87db13b089c8fb53e
jyc <tinkerjyc@gmail.com>
2021-02-08 14:52:47 -0500

    Updated user admin client

---
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -0,0 +1,17 @@
+(function () {
+    var $usernameFld, $passwordFld;
+    var $removeBtn, $editBtn, $createBtn;
+    var $firstNameFld, $lastNameFld, $roleFld;
+    var $userRowTemplate, $tbody;
+    var userService = new AdminUserServiceClient();
+    $(main);
+
+    function main() { … }
+    function createUser() { … }
+    function deleteUser() { … }
+    function selectUser() { … }
+    function updateUser() { … }
+    function renderUsers(users) { … }
+    function findAllUsers() { … } // optional - might not need this
+    function findUserById() { … } // optional - might not need this
+})();

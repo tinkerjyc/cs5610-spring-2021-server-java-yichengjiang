1570fa763fb40605b4b3b3eed3f0ca1248995562
jyc <tinkerjyc@gmail.com>
2021-04-02 21:50:46 -0400

    Fix bug

--- src/main/java/com/example/wbdvsp2102yichengjiangserverjava/services/WidgetService.java
+++ src/main/java/com/example/wbdvsp2102yichengjiangserverjava/services/WidgetService.java
@@ -12,7 +12,7 @@ import java.util.*;
 public class WidgetService {
 
   @Autowired
-  WidgetRepository widgetRepository;
+  WidgetRepository repository;
 
 //     private List<Widget> widgets = new ArrayList<Widget>();
 //    {
@@ -31,19 +31,19 @@ public class WidgetService {
   // implement crud operations
   public Widget createWidget(String tid, Widget widget) {
     widget.setTopicId(tid);
-    return widgetRepository.save(widget);
+    return repository.save(widget);
 //        widget.setId((new Date()).getTime());
 //        widgets.add(widget);
 //        return widget;
   }
 
   public List<Widget> findAllWidgets() {
-    return (List<Widget>) widgetRepository.findAll();
+    return (List<Widget>) repository.findAll();
 //        return widgets;
   }
 
   public List<Widget> findWidgetsForTopic(String tid) {
-    return widgetRepository.findWidgetForTopic(tid);
+    return repository.findWidgetForTopic(tid);
 //        List<Widget> ws = new ArrayList<Widget>();
 //        for(Widget w: widgets) {
 //            if(w.getTopicId().equals(tid)) {
@@ -54,7 +54,7 @@ public class WidgetService {
   }
 
   public Widget findWidgetById(Long wid) {
-    return widgetRepository.findWidgetById(wid);
+    return repository.findWidgetById(wid);
 //        for(Widget w: widgets) {
 //            if(w.getId().equals(wid)) {
 //                return w;
@@ -64,11 +64,52 @@ public class WidgetService {
   }
 
   public int updateWidget(Long id, Widget newWidget) {
-    Widget originalWidget = widgetRepository.findById(id).get();
+    Widget originalWidget = repository.findById(id).get();
+    // TODO: copy all the other fields testing for null
+    if (newWidget.getText() != null) {
+      originalWidget.setText(newWidget.getText());
+    }
+    if (newWidget.getTopicId() != null) {
+      originalWidget.setTopicId(newWidget.getTopicId());
+    }
+    if (newWidget.getName() != null) {
+      originalWidget.setName(newWidget.getName());
+    }
+    if (newWidget.getType() != null) {
+      originalWidget.setType(newWidget.getType());
+    }
+    if (newWidget.getWidgetOrder() != null) {
+      originalWidget.setWidgetOrder(newWidget.getWidgetOrder());
+    }
+    if (newWidget.getCssClass() != null) {
+      originalWidget.setCssClass(newWidget.getCssClass());
+    }
+    if (newWidget.getHeight() != null) {
+      originalWidget.setHeight(newWidget.getHeight());
+    }
+    if (newWidget.getWidth() != null) {
+      originalWidget.setWidth(newWidget.getWidth());
+    }
+    if (newWidget.getSrc() != null) {
+      originalWidget.setSrc(newWidget.getSrc());
+    }
+    if (newWidget.getUrl() != null) {
+      originalWidget.setUrl(newWidget.getUrl());
+    }
+    if (newWidget.getHref() != null) {
+      originalWidget.setHref(newWidget.getHref());
+    }
+    if (newWidget.getSize() != null) {
+      originalWidget.setSize(newWidget.getSize());
+    }
+    if (newWidget.getStyle() != null) {
+      originalWidget.setStyle(newWidget.getStyle());
+    }
+    if (newWidget.getValue() != null) {
+      originalWidget.setValue(newWidget.getValue());
+    }
 
-
-    originalWidget.setText(newWidget.getText());
-    widgetRepository.save(originalWidget);
+    repository.save(originalWidget);
     return 1;
 //        for(int i=0; i<widgets.size(); i++) {
 //            Widget w = widgets.get(i);
@@ -81,7 +122,7 @@ public class WidgetService {
   }
 
   public int deleteWidget(Long id) {
-    widgetRepository.deleteById(id);
+    repository.deleteById(id);
 //        int index = -1;
 //        for(int i=0; i<widgets.size(); i++) {
 //            if(widgets.get(i).getId().equals(id)) {
